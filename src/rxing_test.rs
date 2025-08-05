5b243f3261a9bd696d9866694e1df5a61f467fae
mysteriouspresence <chelseawen03@gmail.com>
2025-08-04 18:14:39 -0700

    reorganize and add code for rustcv

--- src/rxing_test.rs
+++ src/rxing_test.rs
@@ -6,11 +6,13 @@ fn main() {
     let filename = &args[1];
     let try_harder: bool = &args[2] == "true";
     let mut hints = DecodeHints::default().with(rxing::DecodeHintValue::TryHarder(try_harder));
-    let result = detect_in_file_with_hints(
+    let binding = detect_in_file_with_hints(
         filename,
         Some(rxing::BarcodeFormat::DATA_MATRIX),
         &mut hints,
     )
-    .expect("Reading datamatrix: ")
-    .getText();
+    .expect("Reading datamatrix: ");
+    let result = binding.getText();
+
+    println!("Text: {result}");
 }
