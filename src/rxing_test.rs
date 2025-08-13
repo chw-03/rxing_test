79ca3ca200dcf6b17f1fed3fe199b5e15ff68c4e
mysteriouspresence <chelseawen03@gmail.com>
2025-08-13 09:28:57 -0700

    small mods

--- src/rxing_test.rs
+++ src/rxing_test.rs
@@ -1,22 +1,18 @@
 use rxing::{DecodeHints, helpers::*};
-use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let try_harder: bool = &args[2] == "true";
-    let mut hints = DecodeHints::default().with(rxing::DecodeHintValue::TryHarder(try_harder));
+    let mut hints = DecodeHints::default().with(rxing::DecodeHintValue::TryHarder(true));
     let binding = detect_in_file_with_hints(
-        filename,
+        "frame.png",
         Some(rxing::BarcodeFormat::DATA_MATRIX),
         &mut hints,
     );
-    let result: String;
     //.expect("Reading datamatrix: ");
     if let Ok(text) = binding {
-        result = text.getText().to_owned();
-    } else {
-        result = binding.expect_err("Error: ").to_string();
-    }
-    println!("Result: {result}");
+        let result = text.getText().to_owned();
+        println!("Result: {result}");
+    } //else {
+        //result = binding.expect_err("Error: ").to_string();
+    //}
+    //println!("Result: {result}");
 }
--- takephoto.py
+++ takephoto.py
@@ -1,25 +1,28 @@
 from picamera2 import Picamera2, Preview
-import time
+#import time
 import subprocess
 
 side = 500
 cam = Picamera2()
+
 config = cam.create_preview_configuration(
     main={"size": (side, side)}
 )
 
 cam.configure(config)
-cam.start_preview(Preview.DRM)
+cam.start_preview(Preview.DRM, x=1280-side, y=720-side, width=side, height=side,)
 cam.start()
+cam.set_controls({"AfMode": 0, "LensPosition": 0.0358}) #manual mode
+    #Note: Min at 0, Max at 10.0 (distance from 1/0m = infinity to 1/10m = 10 cm)
+    #Here, is set to 11 inches = 27.94 cm --> position = 1/27.94 = 0.0358
 
 try:
     while True:
-        time.sleep(1.0)
-        cam.set_controls({"AfMode": 1 ,"AfTrigger": 0}) #autofocus mode!     
-        print("aaaa")  
-        cam.capture_file("framecap.png")
-        subprocess.run(["./rxing_test", "framecap.png", "true"])
+        #cam.set_controls({"AfMode": 1 ,"AfSpeed":0, "AfTrigger": 0}) #autofocus mode!
+        #start = time.time()     
+        cam.capture_file("frame.png") 
+        subprocess.run(["./rxing_test"]) #filename hardcoded into rust bin
+        #print(f"time to read frame: {time.time() - start}")
 except KeyboardInterrupt:
     cam.stop()
     cam.close()
-
